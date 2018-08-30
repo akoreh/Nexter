e45d93f7526e665b3b9ff2efce7358eddb37a81f
Koreh Andrei <koreh.andrei20@gmail.com>
2018-08-30 16:58:54 +0300

    Built the layout

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -2,9 +2,29 @@ import React from 'react';
 
 import './Layout.css';
 
+import Sidebar from '../../components/Sidebar/Sidebar';
+import Header from '../../components/Header/Header';
+import Realtors from '../../components/Realtors/Realtors';
+import Features from '../../components/Features/Features';
+import Story from '../../components/Story/Story';
+import Homes from '../../components/Homes/Homes';
+import Gallery from '../../components/Gallery/Gallery';
+import Footer from '../../components/Footer/Footer';
+
+import Wrapper from '../../hoc/Wrapper';
+
 const layout = () => {
     return (
-        <h1>Nexter</h1>
+        <Wrapper>
+            <Sidebar />
+            <Header />
+            <Realtors />
+            <Features />
+            <Story />
+            <Homes />
+            <Gallery />
+            <Footer />
+        </Wrapper>
     );
 }
 
